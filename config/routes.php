f23b763062a382add69d4adf88080d4da9c5ed03
rubik16x16 <drt_mike@hotmail.com>
2018-07-13 13:18:30 -0400

    Crear abm de entradas

--- config/routes.php
+++ config/routes.php
@@ -8,6 +8,20 @@ $routes->addGet('/', 'User\IndexController@index', 'user.entradas');
 
 $routes->addGet('/admin', 'Admin\IndexController@index', 'admin.index');
 
+// Entradas
+
 $routes->addGet('/admin/entradas', 'Admin\EntradasController@index', 'admin.entradas.index');
+
+$routes->addGet('/admin/entradas/create', 'Admin\EntradasController@create', 'admin.entradas.create');
+
+$routes->addPost('/admin/entradas', 'Admin\EntradasController@store', 'admin.entradas.store');
+
+$routes->addGet('/admin/entradas/{id}/edit', 'Admin\EntradasController@edit', 'admin.entradas.edit');
+
+$routes->addPut('/admin/entradas/{id}', 'Admin\EntradasController@update', 'admin.entradas.update');
+
+$routes->addGet('/admin/entradas/{id}', 'Admin\EntradasController@show', 'admin.entradas.show');
+
+$routes->addDel('/admin/entradas/{id}', 'Admin\EntradasController@destroy', 'admin.entradas.destroy');
 
 return $routes;
--- src/Controller/Admin/EntradasController.php
+++ src/Controller/Admin/EntradasController.php
@@ -16,4 +16,52 @@ class EntradasController extends Controller{
 
   }
 
+  public function create(){
+
+    return $this->render('admin/entradas/create.html.twig');
+
+  }
+
+  public function store(Request $request){
+
+    $entrada= new Entrada($request->request->all());
+    $entrada->save();
+
+    return $this->redirectToRoute('admin.entradas.index');
+
+  }
+
+  public function edit($id){
+
+    return $this->render('admin/entradas/edit.html.twig', [
+      'entrada' => Entrada::find($id)
+    ]);
+
+  }
+
+  public function update(Request $request, $id){
+
+    $entrada= Entrada::find($id);
+    $entrada->fill($request->request->all());
+    $entrada->save();
+
+    return $this->redirectToRoute('admin.entradas.index');
+
+  }
+
+  public function show($id){
+
+    return new Response('show ' . $id);
+
+  }
+
+  public function destroy($id){
+
+    $entrada= Entrada::find($id);
+    $entrada->delete();
+
+    return new Response('destroy ' . $id);
+
+  }
+
 }
